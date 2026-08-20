b8b4a6dac1fb91ce16aef3689bfee99d1b6285c0
beltransitoooo <migzaz100@gmail.com>
2026-08-20 12:26:32 -0700

    Se agrego fichero de seguridad JWT

---
+++ backend/app/security.py
@@ -0,0 +1,8 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from app import models, database
