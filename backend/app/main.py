f414558ccee78831865b647278aba03e82862e56
beltransitoooo <migzaz100@gmail.com>
2026-08-20 23:34:00 -0700

    Conexion de rutas a main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.routes.auth import router as auth_router
+from app.routes.actividades import router as actividades_router
 
 app = FastAPI()
+
+app.include_router(auth_router)
+app.include_router(actividades_router)
 
 @app.get("/")
 async def root():
