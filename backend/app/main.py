b06f93eb07e979df4d9f6299e1042558c4b7a1bd
beltransitoooo <migzaz100@gmail.com>
2026-09-01 11:23:34 -0700

    Modificacion de schemas para tabla actividades

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], # Cambiar por el dominio del frontend en producción
+    allow_origins=["http://localhost:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -28,26 +28,38 @@ class ActividadBase(BaseModel):
     titulo: str
     descripcion: Optional[str] = None
     completada: bool = False
-
+    tipo_actividad: Optional[str] = "Tarea"
+    prioridad: Optional[str] = "Media"
+    fecha_inicio: Optional[date] = None
+    fecha_vencimiento: Optional[date] = None
+    estimacion_min_hrs: Optional[float] = None
+    estimacion_max_hrs: Optional[float] = None
 
 class ActividadCreate(ActividadBase):
     asignado_a_id: Optional[int] = None
+    lider_tecnico_id: Optional[int] = None
     area_id: Optional[int] = None
-
 
 class ActividadUpdate(BaseModel):
     titulo: Optional[str] = None
     descripcion: Optional[str] = None
     completada: Optional[bool] = None
+    tipo_actividad: Optional[str] = None
+    prioridad: Optional[str] = None
+    fecha_inicio: Optional[date] = None
+    fecha_vencimiento: Optional[date] = None
+    estimacion_min_hrs: Optional[float] = None
+    estimacion_max_hrs: Optional[float] = None
     asignado_a_id: Optional[int] = None
+    lider_tecnico_id: Optional[int] = None
     area_id: Optional[int] = None
-
 
 class ActividadResponse(ActividadBase):
     id: int
     fecha_creacion: datetime
     creador_id: int
     asignado_a_id: Optional[int] = None
+    lider_tecnico_id: Optional[int] = None
     area_id: Optional[int] = None
 
     class Config:
